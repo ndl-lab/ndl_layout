9aa1680fa42896a30b45b34c69bef11603beeea7
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2023-07-12 16:54:41 +0900

    Update cascade_mask_rcnn_convnext-t_p4_w7_fpn_giou_4conv1f_fp16_ms-crop_3x_coco.py

--- models/cascade_mask_rcnn_convnext-t_p4_w7_fpn_giou_4conv1f_fp16_ms-crop_3x_coco.py
+++ models/cascade_mask_rcnn_convnext-t_p4_w7_fpn_giou_4conv1f_fp16_ms-crop_3x_coco.py
@@ -1,36 +1,27 @@
 model = dict(
     type='CascadeRCNN',
-    pretrained='torchvision://resnet50',
+    data_preprocessor = dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_mask=True,
+        pad_size_divisor=32),
     backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch'),
+        type='mmcls.ConvNeXt',
+        arch='tiny',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.4,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained', checkpoint='https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-tiny_3rdparty_32xb128-noema_in1k_20220301-795e9634.pth',
+            prefix='backbone.')),
     neck=dict(
         type='FPN',
-        in_channels=[256, 512, 1024, 2048],
+        in_channels=[96, 192, 384, 768],
         out_channels=256,
         num_outs=5),
-    rpn_head=dict(
-        type='RPNHead',
-        in_channels=256,
-        feat_channels=256,
-        anchor_generator=dict(
-            type='AnchorGenerator',
-            scales=[8],
-            ratios=[0.03125, 0.0625, 0.25, 1.0, 4.0, 16.0, 32.0],
-            strides=[4, 8, 16, 32, 64]),
-        bbox_coder=dict(
-            type='DeltaXYWHBBoxCoder',
-            target_means=[0.0, 0.0, 0.0, 0.0],
-            target_stds=[1.0, 1.0, 1.0, 1.0]),
-        loss_cls=dict(type='EQL', use_sigmoid=True, lambda_=50, version='NDL'),
-        loss_bbox=dict(
-            type='SmoothL1Loss', beta=0.1111111111111111, loss_weight=1.0)),
     roi_head=dict(
         type='CascadeRoIHead',
         num_stages=3,
@@ -42,55 +33,62 @@ model = dict(
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=[
             dict(
-                type='Shared2FCBBoxHead',
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
                 in_channels=256,
+                conv_out_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=15,
+                num_classes=17,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
-                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_means=[0., 0., 0., 0.],
                     target_stds=[0.1, 0.1, 0.2, 0.2]),
-                reg_class_agnostic=True,
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
                 loss_cls=dict(
-                    type='CrossEntropyLoss',
-                    use_sigmoid=False,
-                    loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
-                               loss_weight=1.0)),
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
             dict(
-                type='Shared2FCBBoxHead',
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
                 in_channels=256,
+                conv_out_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=15,
+                num_classes=17,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
-                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_means=[0., 0., 0., 0.],
                     target_stds=[0.05, 0.05, 0.1, 0.1]),
-                reg_class_agnostic=True,
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
                 loss_cls=dict(
-                    type='CrossEntropyLoss',
-                    use_sigmoid=False,
-                    loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
-                               loss_weight=1.0)),
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
             dict(
-                type='Shared2FCBBoxHead',
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
                 in_channels=256,
+                conv_out_channels=256,
                 fc_out_channels=1024,
                 roi_feat_size=7,
-                num_classes=15,
+                num_classes=17,
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
-                    target_means=[0.0, 0.0, 0.0, 0.0],
+                    target_means=[0., 0., 0., 0.],
                     target_stds=[0.033, 0.033, 0.067, 0.067]),
-                reg_class_agnostic=True,
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='SyncBN', requires_grad=True),
                 loss_cls=dict(
-                    type='CrossEntropyLoss',
-                    use_sigmoid=False,
-                    loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0))
         ]),
     train_cfg=dict(
         rpn=dict(
@@ -130,6 +128,7 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
+                mask_size=28,
                 pos_weight=-1,
                 debug=False),
             dict(
@@ -146,6 +145,7 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
+                mask_size=28,
                 pos_weight=-1,
                 debug=False),
             dict(
@@ -162,6 +162,7 @@ model = dict(
                     pos_fraction=0.25,
                     neg_pos_ub=-1,
                     add_gt_as_proposals=True),
+                mask_size=28,
                 pos_weight=-1,
                 debug=False)
         ]),
@@ -174,147 +175,190 @@ model = dict(
         rcnn=dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=5000)))
+            max_per_img=5000,
+            mask_thr_binary=0.5))
+)
+
 dataset_type = 'CocoDataset'
-classes = ('line_main', 'line_inote', 'line_hnote', 'line_caption',
+data_root = 'data/coco/'
+
+image_size = (1024, 1024)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='RandomChoice',
+        transforms=[
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 1024), (512, 1024), (544, 1024), (576, 1024),
+                              (608, 1024), (640, 1024), (672, 1024), (704, 1024),
+                              (736, 1024), (768, 1024), (1024, 1024)],
+                    keep_ratio=True)
+            ],
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(400, 1024), (500, 1024), (600, 1024)],
+                    keep_ratio=True),
+                dict(
+                    type='RandomCrop',
+                    crop_type='absolute_range',
+                    crop_size=(384, 600),
+                    allow_negative_crop=True),
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 1024), (512, 1024), (544, 1024),
+                            (576, 1024), (608, 1024), (640, 1024),
+                            (672, 1024), (704, 1024), (736, 1024),
+                            (768, 1024), (1024, 1024)],
+                    keep_ratio=True)
+            ]
+        ]),
+    dict(type='PackDetInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root='/data/guest_data/ndl/data_coco/',
+        ann_file='/data/guest_data/ndl/data_coco/20230203_preprocessed/gakushu_202303.json',
+        data_prefix=dict(
+                    img='/data/guest_data/ndl/data_coco/20230203_preprocessed'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=None
+    )
+)
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root='/data/guest_data/ndl/data_coco/',
+        ann_file='/data/guest_data/ndl/data_coco/20230203_preprocessed/hyouka_202303_1000.json',
+        data_prefix=dict(
+                    img='/data/guest_data/ndl/data_coco/20230203_preprocessed'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=test_pipeline,
+        backend_args=None
+    )
+)
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root='/data/guest_data/ndl/data_coco/',
+        ann_file='/data/guest_data/ndl/data_coco/20230203_preprocessed/hyouka_202303_1000.json',
+        data_prefix=dict(img='/data/guest_data/ndl/data_coco/20230203_preprocessed'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=test_pipeline,
+        backend_args=None
+    )
+)
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file='/data/guest_data/ndl/data_coco/20230203_preprocessed/hyouka_202303_1000.json',
+    metric=['bbox', 'segm'],
+    format_only=False,
+    backend_args=None)
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file='/data/guest_data/ndl/data_coco/20230203_preprocessed/hyouka_202303_1000.json',
+    metric=['bbox', 'segm'],
+    format_only=False,
+    backend_args=None)
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=500, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=36,
+        by_epoch=True,
+        milestones=[27, 33],
+        gamma=0.1)
+]
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=dict(
+        type='AdamW',
+        lr=0.0024,
+        betas=(0.9, 0.999),
+        weight_decay=0.05
+    ),
+    constructor='LearningRateDecayOptimizerConstructor',
+    paramwise_cfg=dict(
+        decay_rate=0.7, decay_type='layer_wise', num_layers=6)
+)
+
+auto_scale_lr = dict(enable=False, base_batch_size=16)
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook', interval=1, max_keep_ckpts=1),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='DetVisualizationHook'))
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend')],
+    name='visualizer')
+
+log_processor = dict(
+    type='LogProcessor', window_size=50, by_epoch=True)
+
+log_level = 'INFO'
+load_from = 'workdirs/20230206_convnext_allgakushu/latest.pth'
+resume = False
+custom_imports = dict(imports=['mmcls.models'], allow_failed_imports=False)
+checkpoint_file = 'https://download.openmmlab.com/mmclassification/v0/convnext-v2/convnext-v2-tiny_fcmae-in21k-pre_3rdparty_in1k-384px_20230104-d8579f84.pth'
+image_size = (1024, 1024)
+max_epochs = 500
+work_dir = 'workdirs/20230309_convnext_allgakushu'
+seed = 0
+gpu_ids = range(0, 4)
+device = 'cuda'
+metainfo = dict(
+    classes=('line_main', 'line_inote', 'line_hnote', 'line_caption', 'line_ad',
            'block_fig', 'block_table', 'block_pillar', 'block_folio',
            'block_rubi', 'block_chart', 'block_eqn', 'block_cfm', 'block_eng',
-           'char', 'void')
-data_root = 'data/coco/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-image_size = 1024
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(
-        type='Normalize',
-        mean=[123.675, 116.28, 103.53],
-        std=[58.395, 57.12, 57.375],
-        to_rgb=True),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1024, 1024),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(
-                type='Normalize',
-                mean=[123.675, 116.28, 103.53],
-                std=[58.395, 57.12, 57.375],
-                to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
-        ])
-]
-data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
-    train=dict(
-        type='CocoDataset',
-        classes=('line_main', 'line_inote', 'line_hnote', 'line_caption',
-                 'block_fig', 'block_table', 'block_pillar', 'block_folio',
-                 'block_rubi', 'block_chart', 'block_eqn', 'block_cfm',
-                 'block_eng', 'char', 'void'),
-        ann_file=
-        '/mnt/data/ndl/data_coco/20220225_dataset_toppan_preprocessed/data.json',
-        img_prefix=
-        '/mnt/data/ndl/data_coco/20220225_dataset_toppan_preprocessed',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
-            dict(type='RandomFlip', flip_ratio=0.5),
-            dict(
-                type='Normalize',
-                mean=[123.675, 116.28, 103.53],
-                std=[58.395, 57.12, 57.375],
-                to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-        ]),
-    val=dict(
-        type='CocoDataset',
-        classes=('line_main', 'line_inote', 'line_hnote', 'line_caption',
-                 'block_fig', 'block_table', 'block_pillar', 'block_folio',
-                 'block_rubi', 'block_chart', 'block_eqn', 'block_cfm',
-                 'block_eng', 'char', 'void'),
-        ann_file=
-        '/mnt/data/ndl/data_coco/ndl_99_evaluation_rev0805_preprocessed/data.json',
-        img_prefix=
-        '/mnt/data/ndl/data_coco/ndl_99_evaluation_rev0805_preprocessed/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1024, 1024),
-                flip=False,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Normalize',
-                        mean=[123.675, 116.28, 103.53],
-                        std=[58.395, 57.12, 57.375],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='ImageToTensor', keys=['img']),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ]),
-    test=dict(
-        type='CocoDataset',
-        classes=('line_main', 'line_inote', 'line_hnote', 'line_caption',
-                 'block_fig', 'block_table', 'block_pillar', 'block_folio',
-                 'block_rubi', 'block_chart', 'block_eqn', 'block_cfm',
-                 'block_eng', 'char', 'void'),
-        ann_file=
-        '/mnt/data/ndl/data_coco/ndl_99_evaluation_rev0805_preprocessed/data.json',
-        img_prefix=
-        '/mnt/data/ndl/data_coco/ndl_99_evaluation_rev0805_preprocessed/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1024, 1024),
-                flip=False,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Normalize',
-                        mean=[123.675, 116.28, 103.53],
-                        std=[58.395, 57.12, 57.375],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='ImageToTensor', keys=['img']),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ]))
-evaluation = dict(interval=5, metric=['bbox'], classwise=True)
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=None)
-lr_config = dict(policy='step', step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=500)
-checkpoint_config = dict(interval=5)
-log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
-custom_hooks = [dict(type='NumClassCheckHook')]
-dist_params = dict(backend='nccl')
-log_level = 'INFO'
-load_from = None
-workflow = [('train', 1)]
-num_classes = 15
-work_dir = 'train_toppan_20220225/'
-gpu_ids = range(0, 1)
-resume_from = 'train_toppan_20220225/latest.pth'
+           'block_ad', 'text_block', 'text_block_ad'),
+
+)
